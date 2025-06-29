811515192dff5d1329ec2359b032d1c7823870a1
asher09 <sharmaaman0202@gmail.com>
2025-06-30 00:31:02 +0530

    init

---
+++ airdrop/index.ts
@@ -0,0 +1,11 @@
+import {PublicKey, Connection, LAMPORTS_PER_SOL} from "@solana/web3.js"
+
+export const airdrop = async (address: PublicKey, amount: number) => {
+    const publicKey = new PublicKey(address);
+    const conn = new Connection("http://localhost:8899", "confirmed")
+    const signature = await conn.requestAirdrop(publicKey, amount * LAMPORTS_PER_SOL)
+    await conn.confirmTransaction(signature);
+
+}
+
+// airdrop("your-public-key", 5)
