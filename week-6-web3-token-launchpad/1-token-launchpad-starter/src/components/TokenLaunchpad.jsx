9143a833e2980d8c884272909c9bc4d73a6d9c34
Aman <sharmaaman0202@gmail.com>
2026-08-30 23:57:09 +0530

    token launchpad init

--- week-6-web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ week-6-web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -7,6 +7,12 @@ export function TokenLaunchpad() {
     const wallet  = useWallet();
     
     async function createToken() {
+        const name = document.getElementById('name').value;
+        const symbol = document.getElementById('symbol').value;
+        const imageUrl = document.getElementById('imageUrl').value;
+        const initialSupply = document.getElementById('initialSupply').value;
+        
+        
         const lamports = await getMinimumBalanceForRentExemptMint(connection)
         const keypair = Keypair.generate()
 
@@ -32,10 +38,10 @@ export function TokenLaunchpad() {
         flexDirection: 'column'
     }}>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
+        <input className='name' type='text' placeholder='Name'></input> <br />
+        <input className='symbol' type='text' placeholder='Symbol'></input> <br />
+        <input className='imageUrl' type='text' placeholder='Image URL'></input> <br />
+        <input className='initialSupply' type='text' placeholder='Initial Supply'></input> <br />
         <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
